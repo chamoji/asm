a2128c89e14f9acef68cd1b6ab19672a0a9bc6a2
chamoji <westley.mojica@gmail.com>
2015-02-04 15:49:29 -0600

    Final

--- asm.c
+++ asm.c
@@ -9,7 +9,8 @@ void go() {
 int main(int argc, char *argv[]) {
 	char buf[4]; /* store 4 characters (4 bytes) */
 	gets(buf); /* buffer overflow vulnerable... no check for buffer size */
-	printf("buf is at: %p\n", &buf);
+	printf("buf is at:  0x%08x\n", &buf);
+	printf("go() is at: 0x%08x\n", *go);
 	printf("buf contains: %s\n", buf); /* print contents of buf as a string */
 	return 0; /* return zero */
 }
